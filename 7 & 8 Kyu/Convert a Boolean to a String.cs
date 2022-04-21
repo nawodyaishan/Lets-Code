5ec028a4a4f6c26a3b7b5d399de35f695a1b36ea
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-04-21 20:42:04 +0530

    Create Convert a Boolean to a String.cs

---
+++ 7 & 8 Kyu/Convert a Boolean to a String.cs
@@ -0,0 +1,18 @@
+/* Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+ */
+
+
+
+using System;
+
+public class kata
+{
+    public static string boolean_to_string(bool b)
+    {
+        string result = b.ToString();
+        return result;
+    }
+}
