0837d30f107056d9ff3de34ba03e2d6e03c2cb6b
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-31 13:58:46 +0530

    Add Counting Sheep.cs

---
+++ CodeWars/7 & 8 Kyu/Counting Sheep.cs
@@ -0,0 +1,35 @@
+/* Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true, true, true, false,
+  true, true, true, true,
+  true, false, true, false,
+  true, false, false, true,
+  true, true, true, true,
+  false, false, true, true]
+The correct answer would be 17.
+
+Hint: Don 't forget to check for bad values like null/undefined */
+
+
+using System;
+
+public static class Kata
+{
+    public static int CountSheeps(bool[] sheeps)
+    {
+        int counter = 0;
+
+        for (int i = 0; i < sheeps.Length; i++)
+        {
+            if (sheeps[i])
+                counter++;
+        }
+        return counter;
+    }
+}
+
+
+
+
