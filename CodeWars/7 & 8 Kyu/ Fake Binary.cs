04ffd4344e8a4056702bc221566b3055184660d8
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-06-02 22:03:54 +0530

    Add Fake Binary.cs

---
+++ CodeWars/7 & 8 Kyu/ Fake Binary.cs
@@ -0,0 +1,19 @@
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+public class Kata
+{
+    public static string FakeBin(string x)
+    {
+        string result = "";
+        for (int i = 0; i < x.Length; i++)
+        {
+            if (x[i] < 5)
+                result += 0;
+            else if (x[i] >= 5)
+                result += 1;
+        }
+        return result;
+    }
+}
