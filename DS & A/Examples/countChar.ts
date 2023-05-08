aeb9932c5eb6201123f4a7fcb7245334657d23d9
nawodyaishan <nawodyain@gmail.com>
2023-05-08 09:57:09 +0530

    feat: add course update

--- DS & A/Examples/countChar.ts
+++ DS & A/Examples/countChar.ts
@@ -3,6 +3,22 @@
 // charCount("aaaa") {a:4}
 
 
+// function charCount(str: string): Map<string, number> {
+//     // Initialize an empty Map to store the character counts
+//     const charCounts = new Map<string, number>();
+//
+//     // Loop through each character in the input string
+//     for (const char of str) {
+//         // If the character is not in the charCounts Map, add it with a count of 1
+//         // Otherwise, increment the count for that character
+//         charCounts.set(char, (charCounts.get(char) || 0) + 1);
+//     }
+//
+//     // Return the final charCounts Map
+//     return charCounts;
+// }
+
+
 function charCount(str: string): Record<string, number> {
     // Initialize an empty object to store the character counts
     const charCounts: Record<string, number> = {};
