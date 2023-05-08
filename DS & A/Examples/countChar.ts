d731537d912e91bc20ddb93bc4ee83bec7973c20
nawodyaishan <nawodyain@gmail.com>
2023-05-08 09:53:47 +0530

    feat: add course update

--- DS & A/Examples/countChar.ts
+++ DS & A/Examples/countChar.ts
@@ -3,4 +3,19 @@
 // charCount("aaaa") {a:4}
 
 
+function charCount(str: string): Record<string, number> {
+    // Initialize an empty object to store the character counts
+    const charCounts: Record<string, number> = {};
+
+    // Loop through each character in the input string
+    for (const char of str) {
+        // If the character is not in the charCounts object, add it with a count of 1
+        // Otherwise, increment the count for that character
+        charCounts[char] = charCounts[char] ? charCounts[char] + 1 : 1;
+    }
+
+    // Return the final charCounts object
+    return charCounts;
+}
+
 
